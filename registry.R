b37b8ac20ec8e68a6a43b0f04119f9f508a431cb
Jordan Sicherman <jordan.sicherman@msl.ubc.ca>
2021-06-24 02:31:53 -0700

    Apparently I don't get workflows

--- registry.R
+++ registry.R
@@ -771,6 +771,10 @@ doFinalize <- function(document = getOption('gemmaAPI.document', 'R/allEndpoints
   devtools::document()
   devtools::build()
   devtools::install()
+
+  # TODO There are workflows for this but they not working >:(
+  rmarkdown::render('vignettes/Usage.Rmd')
+  knitr::knit('README.Rmd')
 }
 
 doFinalize()
