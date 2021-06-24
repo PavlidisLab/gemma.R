8e8b245491d951f97fa83fd72246830f81156ef1
yavyx <javiercastilloar@gmail.com>
2021-06-24 13:35:23 -0700

    fixed merge conflict in testthat.R

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,10 +1,5 @@
 library(testthat)
-<<<<<<< HEAD
-library(gemmaAPI2)
-
-test_check("gemmaAPI2")
-=======
 library(gemmaAPI)
 
 test_check("gemmaAPI")
->>>>>>> efac9f377db792f7f4d14bd1925db5c040a94d73
+
