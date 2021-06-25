54bd97a8ca203b9911197b38b5f5c1eedd9bda18
yavyx <javiercastilloar@gmail.com>
2021-06-25 14:22:57 -0700

    implemented tests for getDatasets and datasetSearch

--- tests/testthat/test_endpoints.R
+++ tests/testthat/test_endpoints.R
@@ -1,9 +1,23 @@
-library(testthat)
+test_that('getDatasets queries work', {
+  expect_type(getDatasets('GSE2018'), 'list')
+  expect_type(getDatasets('GSE2018', raw = TRUE), 'list')
+  expect_equal(getDatasets(c("GSE2018", "GSE2872")) %>% nrow, 2)
+  expect_equal(getDatasets(limit = 10) %>% nrow, 10)
+  # Offset not working as expected
+  # expect_equal(getDatasets(ofsset = 3) %>% nrow, 7)
+  expect_equal(getDatasets(filter = 'curationDetails.troubled = true')$ee.troubled %>% all, TRUE)
+
+})
 
 test_that('getDatasets queries work', {
   expect_type(getDatasets('GSE2018'), 'list')
   expect_type(getDatasets('GSE2018', raw = TRUE), 'list')
-  # TODO: check implementation to test filters, limit, offset, sort
+  expect_equal(getDatasets(c("GSE2018", "GSE2872")) %>% nrow, 2)
+  expect_equal(getDatasets(limit = 10) %>% nrow, 10)
+  # Offset not working as expected
+  # expect_equal(getDatasets(ofsset = 3) %>% nrow, 7)
+  expect_equal(getDatasets(filter = 'curationDetails.troubled = true')$ee.troubled %>% all, TRUE)
+  expect_equal()
 })
 
 
