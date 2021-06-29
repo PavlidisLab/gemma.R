08d8b44c8061828f50d3b6a9b5a042f49de70645
Jordan Sicherman <jordan.sicherman@msl.ubc.ca>
2021-06-29 14:32:27 -0700

    Added getAnnotation for ermineR. Did not rebuild package, so this will probably fail workflows

--- R/convenience.R
+++ R/convenience.R
@@ -127,3 +127,154 @@ batchId={countN + 1}
     requery(taskID, countN)
   })
 })
+
+#' Get Gemma annotations
+#'
+#' Gets Gemma's platform annotation files that can be accessed from https://gemma.msl.ubc.ca/annots/
+#'
+#' @param platform A platform identifier @seealso getPlatforms
+#' @param annotType Which GO terms should the output include
+#' @param file Where to save the annotation file to, or empty to just load into memory
+#' @param overwrite Whether or not to overwrite an existing file
+#'
+#' @return A table of annotations
+#' @keywords platform
+#' @export
+getAnnotation <- function(platform, annotType = c('bioProcess', 'noParents', 'allParents'),
+                          file = getOption('gemma.file', NA_character_), overwrite = getOption('gemma.overwrite', F)) {
+  if(!is.numeric(platform)) {
+    platforms <- getPlatforms(platform)
+    if(!isTRUE(nrow(platforms) == 1))
+      stop(paste0(platform, ' is not a valid single platform.'))
+    platform <- platforms[, platform.ID]
+  }
+
+  annotType <- match.arg(annotType, c('bioProcess', 'noParents', 'allParents'))
+
+  is.tmp <- is.na(file)
+
+  if(is.na(file))
+    file <- tempfile()
+
+  doReadFile <- function(file) {
+    tmp <- gzfile(file)
+    ret <- tmp %>% readLines %>%
+      .[which(!startsWith(., '#'))[1]:length(.)] %>% # Strip comments
+      paste0(collapse = '\n') %>% {
+        fread(text = .)
+      }
+    close(tmp)
+
+    if(is.tmp)
+      unlink(file)
+
+    ret
+  }
+
+  if(file.exists(file) & !overwrite) {
+    warning(paste0(file, ' exists. Not overwriting.'))
+    doReadFile(file)
+  } else {
+    synchronise({
+      async::http_get(glue::glue(paste0(getOption('gemma.base', 'https://gemma.msl.ubc.ca/'), 'arrays/downloadAnnotationFile.html?id={platform}&fileType={annotType}')), file = file)$then(function(...) {
+        doReadFile(file)
+      })
+    })
+  }
+}
+
+
+  https://gemma.msl.ubc.ca/arrays/downloadAnnotationFile.html?id={pData$id}&fileType={originalAnnotType}
+  REQ1 <- 'callCount=1
+page=/expressionExperiment/showExpressionExperiment.html?id={dataset}
+httpSessionId=
+scriptSessionId=1
+c0-scriptName=ExpressionExperimentDataFetchController
+c0-methodName=getDiffExpressionDataFile
+c0-id=0
+c0-param0=number:{diffExSet}
+batchId=4
+'
+  REQ2 <- 'callCount=1
+page=/expressionExperiment/showExpressionExperiment.html?id={dataset}
+httpSessionId=
+scriptSessionId=1
+c0-scriptName=ProgressStatusService
+c0-methodName=getSubmittedTask
+c0-id=0
+c0-param0=string:{taskID}
+batchId={countN}'
+  REQ3 <- 'callCount=1
+page=/expressionExperiment/showExpressionExperiment.html?id={dataset}
+httpSessionId=
+scriptSessionId=1
+c0-scriptName=TaskCompletionController
+c0-methodName=checkResult
+c0-id=0
+c0-param0=string:{taskID}
+batchId={countN + 1}
+'
+
+  # Make the initial request
+  async::http_post(paste0(getOption('gemma.base', 'https://gemma.msl.ubc.ca/'), 'dwr/call/plaincall/ExpressionExperimentDataFetchController.getDiffExpressionDataFile.dwr'), glue::glue(REQ1))$then(function(response) {
+    if(response$status != 200) {
+      warning(paste0('Unable to get differential expression data file: ', response$status))
+      return(NULL)
+    }
+
+    # Receive a task ID to track
+    taskID <- gsub('\'|"', '', strsplit(gsub('.*remoteHandleCallback\\(([^\\)]+)\\).*', '\\1', rawToChar(response$content)), ',')[[1]])[3]
+    countN <- 5
+
+    requery <- async::async(function(taskID, countN) {
+      # Poll for completion
+      async::http_post(paste0(getOption('gemma.base', 'https://gemma.msl.ubc.ca/'), 'dwr/call/plaincall/ProgressStatusService.getSubmittedTask.dwr'), glue::glue(REQ2))$then(function(response) {
+        if(response$status != 200) {
+          warning(paste0('Unable to get task data: ', response$status))
+          return(NULL)
+        }
+
+        mContent <- rawToChar(response$content)
+        # Detect completion
+        if(grepl('taskStatus:"FAILED"', mContent, fixed = T)) {
+          warning(paste0('Unable to get task data: ', gsub('.*lastLogMessage:"([^"]+)".*', '\\1', mContent)))
+          return(NULL)
+        } else if(grepl('done:true', mContent, fixed = T)) {
+          # Query where to find the result
+          async::http_post(paste0(getOption('gemma.base', 'https://gemma.msl.ubc.ca/'), 'dwr/call/plaincall/TaskCompletionController.checkResult.dwr'), glue::glue(REQ3))$then(function(response) {
+            if(response$status != 200) {
+              warning(paste0('Unable to get task result: ', response$status))
+              return(NULL)
+            }
+
+            # Extract the file location
+            fileLoc <- gsub('\'|"', '', strsplit(gsub('.*remoteHandleCallback\\(([^\\)]+)\\).*', '\\1', rawToChar(response$content)), ',')[[1]])[3]
+
+            # Make a temp file to unzip to
+            tmp <- tempfile()
+            base <- getOption('gemma.base', 'https://gemma.msl.ubc.ca/')
+            async::http_get(paste0(substring(base, 1, nchar(base) - 1), fileLoc), file = tmp)$then(function(...) {
+              filenames <- utils::unzip(tmp, list = TRUE)$Name
+
+              # Unzip results and fread in
+              ret <- lapply(filenames, function(x) data.table::fread(cmd = glue::glue('unzip -p {tmp} {x}'),
+                                                                     colClasses = c(Element_Name = 'character',
+                                                                                    Gene_Symbol = 'character',
+                                                                                    Gene_Name = 'character')))
+              names(ret) <- filenames
+              unlink(tmp)
+              ret
+            })
+          })
+        } else {
+          # Fail to detect completion, try again in 3 seconds
+          async::delay(3)$then(function(...) {
+            requery(taskID, countN + 1)
+          })
+        }
+      })
+    })
+
+    requery(taskID, countN)
+  })
+})
