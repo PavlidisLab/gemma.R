8e18e1687bbfa4de614b82e9b930ca494ca15d3b
Jordan Sicherman <jordan.sicherman@msl.ubc.ca>
2021-07-27 15:47:29 -0700

    Update testthat.R
    I should really think more before committing like this

--- tests/testthat.R
+++ tests/testthat.R
@@ -3,30 +3,6 @@ library(gemmaAPI)
 library(dplyr)
 
 # Prevent certificate issues for GitHub actions
-get_default_curl_options <- function(options) {
-    getopt <- function(nm) {
-        if (!is.null(v <- options[[nm]])) {
-            return(v)
-        }
-        anm <- paste0("async_http_", nm)
-        if (!is.null(v <- getOption(anm))) {
-            return(v)
-        }
-        if (!is.na(v <- Sys.getenv(toupper(anm), NA_character_))) {
-            return(v)
-        }
-    }
-    modifyList(
-        options,
-        list(
-            ssl_verifyhost = 0,
-            ssl_verifypeer = 0, # No verify
-            timeout = as.integer(getopt("timeout") %||% 0),
-            connecttimeout = as.integer(getopt("connecttimeout") %||% 300),
-            low_speed_time = as.integer(getopt("low_speed_time") %||% 0),
-            low_speed_limit = as.integer(getopt("low_speed_limit") %||% 0)
-        )
-    )
-}
+options(ssl.verify = FALSE)
 
 test_check("gemmaAPI")
