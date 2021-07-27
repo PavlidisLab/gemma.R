d23f382beefd44d398d62e6a2d3a4dc457afd776
Jordan Sicherman <jordan.sicherman@msl.ubc.ca>
2021-07-27 15:45:11 -0700

    Update testthat.R
    Fix for internalized async

--- tests/testthat.R
+++ tests/testthat.R
@@ -3,7 +3,7 @@ library(gemmaAPI)
 library(dplyr)
 
 # Prevent certificate issues for GitHub actions
-custom_curl_options <- function(options) {
+default_curl_options <- function(options) {
     getopt <- function(nm) {
         if (!is.null(v <- options[[nm]])) {
             return(v)
@@ -20,7 +20,7 @@ custom_curl_options <- function(options) {
         options,
         list(
             ssl_verifyhost = 0,
-            ssl_verifypeer = 0,
+            ssl_verifypeer = 0, # No verify
             timeout = as.integer(getopt("timeout") %||% 0),
             connecttimeout = as.integer(getopt("connecttimeout") %||% 300),
             low_speed_time = as.integer(getopt("low_speed_time") %||% 0),
@@ -28,7 +28,5 @@ custom_curl_options <- function(options) {
         )
     )
 }
-# environment(custom_curl_options) <- asNamespace('async')
-# assignInNamespace('get_default_curl_options', custom_curl_options, ns = 'async')
 
 test_check("gemmaAPI")
