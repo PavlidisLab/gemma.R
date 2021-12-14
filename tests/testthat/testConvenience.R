a31513d23540f24c707e6c3e2a9a17bed11cb38d
yavyx <javiercastilloar@gmail.com>
2021-12-14 19:15:24 -0400

    fixed getTidyDatasets
    Former-commit-id: e550c3c4f60d5979f8f70c69ff713166117df9aa

--- tests/testthat/testConvenience.R
+++ tests/testthat/testConvenience.R
@@ -12,21 +12,21 @@ test_that("getBioc data is preserved", {
 
     expect_equal(nrow(expr), eset %>% nrow() %>% unname())
     expect_equal(Biobase::featureNames(eset), expr$Probe)
-    expect_equal(Biobase::varLabels(eset), colnames(design[,3:ncol(design)]))
+    expect_equal(Biobase::varLabels(eset), colnames(design))
 
     expect_equal(nrow(expr), nrow(sumexp))
     expect_equal(rownames(sumexp), expr$Probe)
     expect_equal(colnames(SummarizedExperiment::colData(sumexp)),
-                 colnames(design[,3:ncol(design)]))
+                 colnames(design))
 })
 
 test_that("getTidyDataset works properly", {
-    dat <- getDatasetData(1)
+    dat <- getDatasetData(1, filter = TRUE)
     tidy <- getTidyDataset(1)
     design <- getDatasetDesign(1)
     # Check number of rows = samples * probes
-    expect_equal(ncol(dat[, 7:ncol(dat)]) * nrow(dat), nrow(tidy))
+    expect_equal((ncol(dat) - 1) * nrow(dat), nrow(tidy))
     # Check design matrix
-    expect_equal(colnames(design[,3:ncol(design)]), colnames(tidy[,4:ncol(tidy)]))
+    expect_equal(colnames(design), colnames(tidy[, 4:ncol(tidy)]))
 })
 
