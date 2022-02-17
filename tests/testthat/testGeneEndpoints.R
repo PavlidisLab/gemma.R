2556baea34370f9bb3da41628730123b16b872e0
yavyx <javiercastilloar@gmail.com>
2022-02-17 14:54:50 -0800

    removed GeneEvidence endpoint
    Former-commit-id: b3239cf6ef2df5c425fe0cce17ac50f5080d4dbc

--- tests/testthat/testGeneEndpoints.R
+++ tests/testthat/testGeneEndpoints.R
@@ -9,24 +9,6 @@ test_that("getGenesInfo queries work", {
     )
 })
 
-test_that("getGeneEvidence queries work", {
-    dat <- getGeneEvidence(1859)
-    raw <- getGeneEvidence(1859, raw = TRUE)
-    expect_type(dat, "list")
-    expect_type(raw, "list")
-    expect_equal(
-        dat[, c(gene.Symbol, gene.Ensembl, gene.Name, taxon.Name)],
-        c(raw$officialSymbol, raw$ensemblId, raw$officialName, raw$taxonCommonName)
-    )
-
-    datEv <- dat$evidence[[1]]
-    rawEv <- raw$evidence[[1]]$phenotypeAssPubVO[[1]]$citationValueObject
-    expect_equal(
-        datEv[1, c(pubmed.URL, citation, pubmed.Accession)],
-        c(rawEv$pubmedURL, rawEv$citation, rawEv$pubmedAccession)
-    )
-})
-
 test_that("getGeneLocation queries work", {
     dat <- getGeneLocation(1859)
     raw <- getGeneLocation(1859, raw = TRUE)
