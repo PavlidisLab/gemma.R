9f14d02ddeb58b6274de9b28798d1804f0d5228a
yavyx <javiercastilloar@gmail.com>
2022-02-27 18:27:44 -0800

    skipping long running tests to pass RMD/Bioc checks

--- tests/testthat/testDatasetEndpoints.R
+++ tests/testthat/testDatasetEndpoints.R
@@ -76,6 +76,9 @@ test_that("datasetAnnotations queries work", {
 })
 
 test_that("getDatasetExpression queries work", {
+    # Skip during checks since long runtime causes RMD/biocCheck fails
+    skip_on_cran()
+    skip_on_bioc()
     dat <- expect_type(getDatasetExpression(1), "list")
     expect_gt(getDatasetExpression(1) %>% nrow(), getDatasetExpression(1, filter = TRUE) %>% nrow())
 })
