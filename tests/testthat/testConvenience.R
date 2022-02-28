d4fe1e8fba698ea0e461e4fbfea8a7df57991c8d
yavyx <javiercastilloar@gmail.com>
2022-02-27 18:27:44 -0800

    skipping long running tests to pass RMD/Bioc checks

--- tests/testthat/testConvenience.R
+++ tests/testthat/testConvenience.R
@@ -5,6 +5,9 @@ test_that("getPlatformAnnotation queries work", {
 })
 
 test_that("getDataset works properly", {
+    # Skip during checks since long runtime causes RMD/biocCheck fails
+    skip_on_cran()
+    skip_on_bioc()
     expr <- getDatasetExpression(1, filter = TRUE)
     eset <- getDataset("GSE2018", filter = TRUE, type = "eset")
     sumexp <- getDataset("GSE2018", filter = TRUE, type = "se")
@@ -23,6 +26,9 @@ test_that("getDataset works properly", {
 })
 
 test_that("getDatasetTidy works properly", {
+    # Skip during checks since long runtime causes RMD/biocCheck fails
+    skip_on_cran()
+    skip_on_bioc()
     dat <- getDatasetExpression(1)
     tidy <- getDatasetTidy(1)
     design <- getDatasetDesign(1)
