925b8166d16fb86b9baeb67ce5dff2a88ff22b8e
OganM <ogan.mancarcii@gmail.com>
2022-05-10 14:51:47 -0700

    force cache creation

--- R/aa_caching.R
+++ R/aa_caching.R
@@ -5,6 +5,7 @@ gemmaCache <- function(){
   dir <- getOption(x = "gemma.cache",
                    tools::R_user_dir('gemmaR', which="cache"))
 
+  dir.create(dir,recursive = TRUE,showWarnings = FALSE)
   memoise::cache_filesystem(dir, algo = "xxhash64", compress = FALSE)
 }
 
