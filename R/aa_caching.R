a28283990488e1e0f3be9db24d78ab9e1e7b62d1
OganM <ogan.mancarcii@gmail.com>
2022-05-17 10:29:08 -0700

    backwards compatibility for older R versions

--- R/aa_caching.R
+++ R/aa_caching.R
@@ -1,9 +1,9 @@
 #' Gemma Cache
-#'
+#' @return A memoise filesystem
 #' @keywords internal
 gemmaCache <- function(){
   dir <- getOption(x = "gemma.cache",
-                   tools::R_user_dir('gemmaR', which="cache"))
+                   rappdirs::user_cache_dir(appname ='gemmaR'))
 
   dir.create(dir,recursive = TRUE,showWarnings = FALSE)
   memoise::cache_filesystem(dir, algo = "xxhash64", compress = FALSE)
