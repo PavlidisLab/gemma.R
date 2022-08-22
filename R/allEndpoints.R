510d4f1b77d406b9cb567522cde81a8ea124f3e1
OganM <ogan.mancarci@gmail.com>
2022-08-21 18:23:15 -0700

    parameter override and taxon endpoint

--- R/allEndpoints.R
+++ R/allEndpoints.R
@@ -30,11 +30,11 @@
 #' @param file The name of a file to save the results to, or `NULL` to not write results to a file. If `raw == TRUE`, the output will be a JSON file. Otherwise, it will be a RDS file.
 #' @param overwrite Whether or not to overwrite if a file exists at the specified filename.
 #'
-#' @return A data table with information about the queried dataset(s). Returns an empty list if no datasets matched.
-#' A successful response may contain 'Geeq'
-#' information, which aims to provide a unified metric to measure
-#' experiments by the quality of their data, and their suitability for use
-#' in Gemma. You can [read more about the geeq properties here](https://pavlidislab.github.io/Gemma/geeq.html)
+#' @return A data table with information about the queried dataset(s). Returns
+#' an empty list if no datasets matched. A successful response may contain 'Geeq'
+#' information, which aims to provide a unified metric to measure experiments by
+#' the quality of their data, and their suitability for use in Gemma. You can
+#' \href{https://pavlidislab.github.io/Gemma/geeq.html}{read more about the geeq properties here}
 #' @export
 #'
 #' @keywords dataset
@@ -1529,6 +1529,88 @@ memsearchAnnotations <- function(query, raw = getOption("gemma.raw", FALSE), mem
     mem_call <- memoise::memoise(searchAnnotations, cache = gemmaCache())
     mem_call(
         query = query, raw = raw, memoised = FALSE, file = file,
+        overwrite = overwrite
+    )
+}
+
+#' Annotation search
+#'
+#' Does a search for annotations based on the given string
+#'
+#' @param taxa Limits the result to entities with given identifiers.
+#' A vector of identifiers.
+#' Identifiers can be the any of the following:
+#' \itemize{
+#' \item taxon ID
+#' \item scientific name
+#' \item common name
+#' Retrieval by ID is more efficient.
+#' Do not combine different identifiers in one query.
+#' For convenience, below is a list of officially supported taxa
+#' \tabular{rllr}{
+#' \strong{ID} \tab \strong{Comm.name} \tab \strong{Scient.name}    \tab \strong{NcbiID}\cr
+#' 1            \tab human               \tab Homo sapiens             \tab 9606            \cr
+#' 2            \tab mouse               \tab Mus musculus             \tab 10090           \cr
+#' 3            \tab rat                 \tab Rattus norvegicus        \tab 10116           \cr
+#' 11           \tab yeast               \tab Saccharomyces cerevisiae \tab 4932            \cr
+#' 12           \tab zebrafish           \tab Danio rerio              \tab 7955            \cr
+#' 13           \tab fly                 \tab Drosophila melanogaster  \tab 7227            \cr
+#' 14           \tab worm                \tab Caenorhabditis elegans   \tab 6239
+#' }
+#' }
+#' @param raw `TRUE` to receive results as-is from Gemma, or `FALSE` to enable parsing. Raw results usually contain additional fields and flags that are omitted in the parsed results.
+#' @param memoised Whether or not to save to cache for future calls with the same inputs and use the result saved in cache if a result is already saved. Doing `options(gemma.memoised = TRUE)` will ensure that the cache is always used. Use \code{\link{forgetGemmaMemoised}} to clear the cache.
+#' @param file The name of a file to save the results to, or `NULL` to not write results to a file. If `raw == TRUE`, the output will be a JSON file. Otherwise, it will be a RDS file.
+#' @param overwrite Whether or not to overwrite if a file exists at the specified filename.
+#'
+#' @return A data table with the queried taxa's details.
+#' @export
+#'
+#' @keywords taxon
+#'
+#' @examples
+#' getTaxonInfo(c("mouse", "human"))
+getTaxonInfo <- function(taxa, raw = getOption("gemma.raw", FALSE), memoised = getOption(
+        "gemma.memoised",
+        FALSE
+    ), file = getOption("gemma.file", NA_character_), overwrite = getOption(
+        "gemma.overwrite",
+        FALSE
+    )) {
+    internal <- FALSE
+    keyword <- "taxon"
+    header <- ""
+    isFile <- FALSE
+    fname <- "getTaxonInfo"
+    preprocessor <- processTaxon
+    validators <- list(taxa = validateTaxa)
+    endpoint <- "taxa/{encode(taxa)}"
+    if (memoised) {
+        out <- memgetTaxonInfo(
+            taxa = taxa, raw = raw, memoised = FALSE,
+            file = file, overwrite = overwrite
+        )
+        return(out)
+    }
+    .body(
+        fname, validators, endpoint, environment(), isFile,
+        header, raw, overwrite, file, match.call()
+    )
+}
+
+#' Memoise getTaxonInfo
+#'
+#' @noRd
+memgetTaxonInfo <- function(taxa, raw = getOption("gemma.raw", FALSE), memoised = getOption(
+        "gemma.memoised",
+        FALSE
+    ), file = getOption("gemma.file", NA_character_), overwrite = getOption(
+        "gemma.overwrite",
+        FALSE
+    )) {
+    mem_call <- memoise::memoise(getTaxonInfo, cache = gemmaCache())
+    mem_call(
+        taxa = taxa, raw = raw, memoised = FALSE, file = file,
         overwrite = overwrite
     )
 }
---
+++ inst/script/overrides.R
@@ -0,0 +1,44 @@
+# use this file to override automatically generated documentation elements.
+# supported elements are title, examples, parameters and the return value.
+# add a NULL at the end of the documentation block to allow roxygen to parse
+
+#' getDatasetsInfo
+#' @examples
+#' getDatasetsInfo("GSE2018")
+#' getDatasetsInfo(c("GSE2018", "GSE2872"))
+#' @return A data table with information about the queried dataset(s). Returns
+#' an empty list if no datasets matched. A successful response may contain 'Geeq'
+#' information, which aims to provide a unified metric to measure experiments by
+#' the quality of their data, and their suitability for use in Gemma. You can
+#' [read more about the geeq properties here](https://pavlidislab.github.io/Gemma/geeq.html)
+NULL
+
+
+#' getTaxonInfo
+#' @param taxa Limits the result to entities with given identifiers.
+#' A vector of identifiers.
+#' Identifiers can be the any of the following:
+#' -   taxon ID
+#' -   scientific name
+#' -   common name
+#' Retrieval by ID is more efficient.
+#' Do not combine different identifiers in one query.
+#' For convenience, below is a list of officially supported taxa
+#' \tabular{rllr}{
+#'     \strong{ID} \tab \strong{Comm.name} \tab \strong{Scient.name}    \tab \strong{NcbiID}\cr
+#'     1            \tab human               \tab Homo sapiens             \tab 9606            \cr
+#'    2            \tab mouse               \tab Mus musculus             \tab 10090           \cr
+#'    3            \tab rat                 \tab Rattus norvegicus        \tab 10116           \cr
+#'    11           \tab yeast               \tab Saccharomyces cerevisiae \tab 4932            \cr
+#'    12           \tab zebrafish           \tab Danio rerio              \tab 7955            \cr
+#'    13           \tab fly                 \tab Drosophila melanogaster  \tab 7227            \cr
+#'    14           \tab worm                \tab Caenorhabditis elegans   \tab 6239
+#'}
+#' @examples
+#' getTaxonInfo(c('mouse','human'))
+#' @return A data table with the queried taxa's details.
+NULL
+
+
+#' generic_params
+NULL
