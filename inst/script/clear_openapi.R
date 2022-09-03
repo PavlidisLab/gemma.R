382f853d79fc4a784a94ae15afce97ac9063d6ef
OganM <ogan.mancarci@gmail.com>
2022-09-02 18:49:13 -0700

    cleanup code not needed

--- inst/script/clear_openapi.R
+++
@@ -1,6 +0,0 @@
-auto_gen_files = readLines('inst/script/auto_gen_code/.openapi-generator/FILES')
-R_files = auto_gen_files[grepl('^R/',auto_gen_files)]
-unlink('inst/script/auto_gen_code/',recursive = TRUE)
-R_files %>% sapply(function(x){
-    unlink(x)
-})
