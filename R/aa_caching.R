81d463357b3558726c676d4bdce3ac52ea53a430
OganM <ogan.mancarcii@gmail.com>
2022-09-13 13:06:11 -0700

    allow caching in memory instead of the filesystem

--- R/aa_caching.R
+++ R/aa_caching.R
@@ -5,6 +5,9 @@ gemmaCache <- function(){
   dir <- getOption(x = "gemma.cache",
                    rappdirs::user_cache_dir(appname ='gemmaR'))
 
+  if(dir == "cache_in_memory"){
+      return(memoise::cache_memory())
+  }
   dir.create(dir,recursive = TRUE,showWarnings = FALSE)
   memoise::cache_filesystem(dir, algo = "xxhash64", compress = FALSE)
 }
