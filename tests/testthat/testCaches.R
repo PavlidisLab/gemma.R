ddc7a8d64aa445480c3bf7ebbb37f24186d295ce
OganM <ogan.mancarcii@gmail.com>
2022-09-15 18:30:09 -0700

    more caching tests

--- tests/testthat/testCaches.R
+++ tests/testthat/testCaches.R
@@ -1,5 +1,5 @@
 
-test_that('caches work',{
+test_that('caches work for base functions',{
     forget_gemma_memoised()
 
   timeNonMemo =
@@ -26,8 +26,13 @@ test_that('caches work',{
 
   testthat::expect_lt(timeMemo$mean,timeForgot$mean)
   testthat::expect_lt(timeMemo$mean,timeNonMemo$mean)
+})
 
-  # testing caches for high level functions
+forget_gemma_memoised()
+options(gemma.memoised = FALSE)
+options(gemma.cache = NULL)
+
+test_that('caches work for high level functions',{
   skip_on_ci()
   skip_on_bioc()
 
@@ -59,7 +64,40 @@ test_that('caches work',{
   testthat::expect_lt(timeMemo$mean,timeNonMemo$mean)
   testthat::expect_lt(optionMemo$mean,timeNonMemo$mean)
 
-  # test custom caches
+  
+  # test for get platform annotation's memoisation since it's not auto-generated with the rest
+  timeNonMemo =
+      microbenchmark::microbenchmark(
+          get_platform_annotations(1,memoised = FALSE),
+          times = 1,unit = 'ms') %>% summary
+  
+  result = get_platform_annotations(1, memoise = TRUE)
+  
+  timeMemo =
+      microbenchmark::microbenchmark(
+          get_platform_annotations(1, memoise = TRUE),
+          times = 1,unit = 'ms') %>% summary
+  
+  forget_gemma_memoised()
+  
+  timeForgot =
+      microbenchmark::microbenchmark(
+          get_platform_annotations(1, memoise = TRUE),
+          times = 1,unit = 'ms') %>% summary
+  
+  
+  testthat::expect_lt(timeMemo$mean,timeForgot$mean)
+  testthat::expect_lt(timeMemo$mean,timeNonMemo$mean)
+})
+
+forget_gemma_memoised()
+options(gemma.memoised = FALSE)
+options(gemma.cache = NULL)
+
+test_that('custom cache paths work',{
+  # test custom cache
+  test_gse = 'GSE2018'
+    
   non_default = tempfile()
   options(gemma.cache = non_default)
   forget_gemma_memoised()
@@ -79,6 +117,8 @@ test_that('caches work',{
   expect_warning(get_platforms_by_ids(1,file = tempfile(),memoised = TRUE),'Saving to files is not supported')
 
 })
+
+
 # don't forget to unmemoise at the end
 forget_gemma_memoised()
 options(gemma.memoised = FALSE)
@@ -113,6 +153,31 @@ test_that('in memory caches work',{
     
     testthat::expect_lt(timeMemo$mean,timeForgot$mean)
     testthat::expect_lt(timeMemo$mean,timeNonMemo$mean)
+    
+    # test for get platform annotation's memoisation since it's not auto-generated with the rest
+    timeNonMemo =
+        microbenchmark::microbenchmark(
+            get_platform_annotations(1,memoised = FALSE),
+            times = 1,unit = 'ms') %>% summary
+    
+    result = get_platform_annotations(1, memoise = TRUE)
+    
+    timeMemo =
+        microbenchmark::microbenchmark(
+            get_platform_annotations(1, memoise = TRUE),
+            times = 1,unit = 'ms') %>% summary
+    
+    forget_gemma_memoised()
+    
+    timeForgot =
+        microbenchmark::microbenchmark(
+            get_platform_annotations(1, memoise = TRUE),
+            times = 1,unit = 'ms') %>% summary
+    
+    
+    testthat::expect_lt(timeMemo$mean,timeForgot$mean)
+    testthat::expect_lt(timeMemo$mean,timeNonMemo$mean)
 })
 forget_gemma_memoised()
 options(gemma.cache = NULL)
+options(gemma.memoised = FALSE)
