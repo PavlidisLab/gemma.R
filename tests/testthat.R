b2d166ef07dda3db3b52564d76b0e79658afd95e
OganM <ogan.mancarci@gmail.com>
2022-10-12 10:45:34 -0700

    change default test to non-dev

--- tests/testthat.R
+++ tests/testthat.R
@@ -4,7 +4,7 @@ library(dplyr)
 # Prevent certificate issues for GitHub actions
 options(gemma.SSL = FALSE)
 # get gemma api path if it's set in the environment
-options(gemma.API = Sys.getenv('API_PATH','https://dev.gemma.msl.ubc.ca/rest/v2/'))
+options(gemma.API = Sys.getenv('API_PATH','https://gemma.msl.ubc.ca/rest/v2/'))
 print(gemma.R:::gemmaPath())
 test_check("gemma.R")
 
