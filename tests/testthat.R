75a23f4d5df586986013bad78b5d940494e6eb58
OganM <ogan.mancarci@gmail.com>
2024-01-26 12:17:38 -0800

    minor

--- tests/testthat.R
+++ tests/testthat.R
@@ -4,7 +4,7 @@ library(dplyr)
 # Prevent certificate issues for GitHub actions
 options(gemma.SSL = FALSE)
 # get gemma api path if it's set in the environment
-setGemmaPath('prod')
+gemsetGemmaPath('prod')
 print(gemma.R:::gemmaPath())
 test_check("gemma.R")
 
