64262436f95240f82c04056487fae665e9a5e938
OganM <ogan.mancarcii@gmail.com>
2024-02-09 17:12:02 -0800

    single function to set memoisation options

--- R/aa_caching.R
+++ R/aa_caching.R
@@ -19,11 +19,17 @@ in_memory_cache <- function(...){
 
 #' Enable and disable memoisation of gemma.R functions
 #' @param memoised boolean. If TRUE memoisation will be enabled
-#' @return Option set for memoisation
-#' @keywords internal
-gemma_memoise <- function(memoised = NULL){
-    options('gemma.memoised' = setting)
-    getOption('gemma.memoised',FALSE)
+#' @param cache File path or "cache_in_memory". File path will chose a location
+#' to save the cache files for memoisation. "cache_in_memory" will store the cache
+#' in the current R session
+#' @return NULL
+#' @keywords misc
+#' @export
+gemma_memoise <- function(memoised = FALSE,
+                          cache = rappdirs::user_cache_dir(appname ='gemmaR')){
+    options('gemma.memoised' = memoised)
+    options('gemma.cache' = cache)
+    NULL
 }
 
 mem_in_memory_cache <- memoise::memoise(in_memory_cache, cache = memoise::cache_memory())
