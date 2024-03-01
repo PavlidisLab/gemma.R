cd545a1b3e38b32fbe5effeabd2f876b573c6060
OganM <ogan.mancarcii@gmail.com>
2024-02-29 18:19:30 -0800

    improve query test

--- tests/testthat/testMisc.R
+++ tests/testthat/testMisc.R
@@ -10,12 +10,20 @@ test_that("test query formation",{
     # tests for removal of empty elements inlcusion of filter parameters
     res_dataset <- get_result_sets(1)
     
-    query_string <- attributes(res_dataset)$call %>% strsplit('\\?') %>% {.[[1]]} %>% {.[[2]]}
-    testthat::expect_true(query_string == "datasets=1&offset=0&limit=20&sort=%2Bid")
-    
+    query_string <- attributes(res_dataset)$call %>% 
+        strsplit('\\?') %>% 
+        {.[[1]]} %>% 
+        {.[[2]]} %>%
+        shiny::parseQueryString()
+    testthat::expect_true(query_string$datasets == 1)
+
     res_result_set <- get_result_sets(resultSets = res_dataset$result.ID)
-    query_string <- attributes(res_result_set)$call %>% strsplit('\\?') %>% {.[[1]]} %>% {.[[2]]}
-    testthat::expect_true(query_string == glue::glue("filter=id%20in%20%28{res_dataset$result.ID}%29&offset=0&limit=20&sort=%2Bid"))
+    query_string <- attributes(res_result_set)$call %>% strsplit('\\?') %>%
+        {.[[1]]} %>%
+        {.[[2]]} %>% 
+        shiny::parseQueryString()
+    testthat::expect_true(!'dataset' %in% names(query_string))
+    testthat::expect_true(query_string$filter == glue::glue('id in ({res_dataset$result.ID})'))
     
     
 })
